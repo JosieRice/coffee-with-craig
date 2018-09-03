feb4360221e691dc197126036b7dd3c06cd9c438
Craig Rice <craigricetong@gmail.com>
2018-09-03 14:15:37 -0700

    changes to link for calendly

--- client/src/App.js
+++ client/src/App.js
@@ -23,16 +23,29 @@ class App extends Component {
   };
 
   render() {
-    const serverInfo = this.state.response;
+    // const serverInfo = this.state.response;
+    const calendlyStyle = {
+      minWidth: '320px',
+      height: '580px',
+    };
     
     return (
+
       <div className="App">
         <header className="App-header">
+        {/* <nav>Coffee Skills Interests Portfolio</nav> */}
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Welcome to Coffee with Craig</h1>
-          <p className="App-intro">you can reach me at <a href="mailto:craigricetong@gmail.com?subject=Let's get some coffee.">craigricetong@gmail.com</a> while this app is under construction</p>
+          {/* <p className="App-intro">you can reach me at <a href="mailto:craigricetong@gmail.com?subject=Let's get some coffee.">craigricetong@gmail.com</a> while this app is under construction</p> */}
           {/* <p className="App-intro">{serverInfo}</p> */}
         </header>
+        
+          
+          {/* <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd_6FKnga_YV8zthlZqZPYVh9-V7fpxAivXdg7kZfyOdf3Mdw/viewform?embedded=true" width="700" height="520" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe> */}
+
+          <a href="https://calendly.com/coffeewithcraig/30min" className="CWC-Link" target="_blank"><h1>Let's meet up for some coffee.</h1></a>
+
+
       </div>
     );
   }
